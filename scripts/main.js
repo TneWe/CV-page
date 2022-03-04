b9bd8043a32205adc30f5a2d7cd6c035066b4689
TneWe <tnevesca@gmail.com>
2022-03-04 11:42:34 +0100

    ispravke

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/toni.png') {
-      myImage.setAttribute('src','images/toni2.png');
+    if(mySrc === 'images/toni3.png') {
+      myImage.setAttribute('src','images/toni4.png');
     } else {
-      myImage.setAttribute('src','images/toni.png');
+      myImage.setAttribute('src','images/toni3.png');
     }
 }
