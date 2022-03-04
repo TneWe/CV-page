0aaa5888494ab45ef57af99438a522e7d12a2798
TneWe <tnevesca@gmail.com>
2022-03-04 12:51:41 +0100

    last commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 let myImage = document.querySelector('img');
-
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
+    let mySrc = myImage.get('src');
     if(mySrc === 'images/toni3.png') {
       myImage.setAttribute('src','images/toni4.png');
     } else {
